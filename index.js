09cfbc7e5e35420f4c6bc1344121def7f4c900cd
Freddy Florial <freddyflorial@gmail.com>
2022-08-04 20:29:36 -0400

    commit tree 6b5a002d05a73a60a95637c44127559e3706607b

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // code your solution here
+function superbowlWin(array){
+    let findResult = array.find(item => item.result === 'W');
+    
+    if(findResult) {
+        return findResult.year;
+    }
+}
